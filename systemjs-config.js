c552ed7ed6ba5d515eaaaa320b558486e42729b2
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-04 20:14:46 +0100

    BP-ATS: Added support for JSON plugin

--- systemjs-config.js
+++ systemjs-config.js
@@ -5,8 +5,7 @@
         "ngSanitize": "node_modules/angular-sanitize",
         "ngRoute": "node_modules/angular-route",
         "ngStorage": "node_modules/ngstorage/",
-        "bootstrap": "node_modules/bootstrap/dist/js/",
-        "jquery": "node_modules/jquery/dist/"
+        "json": "node_modules/systemjs-plugin-json/"
     };
 
     var packages = {
@@ -15,8 +14,7 @@
         "ngSanitize": { main: "index.js", defaultExtension: "js" },
         "ngRoute": { main: "index.js", defaultExtension: "js" },
         "ngStorage": { main: "ngStorage.js", defaultExtension: "js" },
-        "bootstrap": { main: "bootstrap.js", defaultExtension: "js" },
-        "jquery": { main: "jquery.js", defaultExtension: "js" }
+        "json": { main: "json.js", defaultExtension: "js" }
     };
 
     var config = {
