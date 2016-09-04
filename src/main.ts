ced36517afa08bc06336ab36cc07dd0723258d82
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-04 21:20:29 +0100

    BP-ATS: Updated main.ts with angular-ui-bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import * as angular from "angular";
 import "ngSanitize";
 import "ngRoute";
 import "ngStorage";
+import "ngAnimate";
+import "ngTouch";
+import "angular-ui-bootstrap";
 import { HomeController } from "./home.controller/home.controller";
 import { DepartmentController } from "./department.controller/department.controller";
 import { DepartmentService } from "./department.service/department.service";
@@ -9,7 +12,7 @@ import { registerRoutesFor } from "./routes";
 
 export module app {
     "use strict";
-    var app = angular.module("app", ["ngSanitize", "ngRoute", "ngStorage"])
+    var app = angular.module("app", ["ngSanitize", "ngRoute", "ngStorage", "ngAnimate", "ngTouch", "ui.bootstrap"])
                 .controller("homeController", HomeController)
                 .controller("departmentController", DepartmentController)
                 .factory("departmentService", [DepartmentService]);
