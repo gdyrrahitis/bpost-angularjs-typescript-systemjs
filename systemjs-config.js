461b099ec15e016a741f013bed5aacbd46f40473
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-04 21:19:48 +0100

    BP-ATS: Updated systemjs-config with additional packages

--- systemjs-config.js
+++ systemjs-config.js
@@ -2,9 +2,12 @@
     var map = {
         "app": "src",
         "angular": "node_modules/angular",
-        "ngSanitize": "node_modules/angular-sanitize",
-        "ngRoute": "node_modules/angular-route",
+        "ngSanitize": "node_modules/angular-sanitize/",
+        "ngRoute": "node_modules/angular-route/",
         "ngStorage": "node_modules/ngstorage/",
+        "ngAnimate": "node_modules/angular-animate/",
+        "ngTouch": "node_modules/angular-touch/",
+        "angular-ui-bootstrap": "node_modules/angular-ui-bootstrap/",
         "json": "node_modules/systemjs-plugin-json/"
     };
 
@@ -13,6 +16,9 @@
         "angular": { main: "index.js", defaultExtension: "js" },
         "ngSanitize": { main: "index.js", defaultExtension: "js" },
         "ngRoute": { main: "index.js", defaultExtension: "js" },
+        "ngAnimate": { main: "index.js", defaultExtension: "js" },
+        "ngTouch": { main: "index.js", defaultExtension: "js" },
+        "angular-ui-bootstrap": { main: "index.js", defaultExtension: "js" },
         "ngStorage": { main: "ngStorage.js", defaultExtension: "js" },
         "json": { main: "json.js", defaultExtension: "js" }
     };
