9465d98d6914370865a4202198347a508a152833
George Dyrrachitis <GeorgeD@cubictelecom.com>
2016-09-05 09:17:24 +0100

    BP-ATS: Added modal controller in application module

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import "angular-ui-bootstrap";
 import { HomeController } from "./home.controller/home.controller";
 import { DepartmentController } from "./department.controller/department.controller";
 import { DepartmentService } from "./department.service/department.service";
+import { EmployeeModalController } from "./employee.modal.controller/employee.modal.controller";
 import { registerRoutesFor } from "./routes";
 
 export module app {
@@ -15,6 +16,7 @@ export module app {
     var app = angular.module("app", ["ngSanitize", "ngRoute", "ngStorage", "ngAnimate", "ngTouch", "ui.bootstrap"])
                 .controller("homeController", HomeController)
                 .controller("departmentController", DepartmentController)
+                .controller("employeeModalController", EmployeeModalController)
                 .factory("departmentService", [DepartmentService]);
     
     registerRoutesFor(app);
