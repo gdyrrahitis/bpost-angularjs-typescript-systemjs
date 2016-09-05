3a28b18da63206cf67615ba0bb657999a96471b2
George Dyrrachitis <GeorgeD@cubictelecom.com>
2016-09-05 09:25:35 +0100

    BP-ATS: Added appconfig.d.ts definition

---
+++ typings/custom/appconfig/appconfig.d.ts
@@ -0,0 +1,33 @@
+declare module AppConfig {
+    interface Route {
+        path: string;
+        templateUrl: string; 
+        controller: string;
+    }
+
+    interface Department {
+        name: string;
+        maxAllowedEmployees: number;
+    }
+    
+    interface Modal {
+        animation: boolean;
+        size: string;
+        ariaLabelledBy: string;
+        ariaDescribedBy: string;
+        templateUrl: string;
+        controller: string;
+        controllerAs: string;
+    }
+
+    interface Client {
+        basePath: string;
+        routes: Route[];
+        departments: Department[],
+        modal: Modal
+    }
+
+    export interface Configuration {
+        client: Client;
+    }
+}
