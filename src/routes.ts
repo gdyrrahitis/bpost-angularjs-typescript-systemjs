e9455f6fba2e7d93337b74d6e05b63fc249d9ba3
George Dyrrachitis <GeorgeD@cubictelecom.com>
2016-09-06 16:25:49 +0100

    BP-ATS: Updated code to use strongly typed $routeProvider for angular

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import "angular";
 export function registerRoutesFor(app: angular.IModule) {
     "use strict";
 
-    app.config(($routeProvider: any, $locationProvider: angular.ILocationProvider) => {
+    app.config(($routeProvider: angular.route.IRouteProvider, $locationProvider: angular.ILocationProvider) => {
         $locationProvider.html5Mode(config.client.html5Mode);
 
         let home = config.client.routes.find(v => v.controller === "homeController");
